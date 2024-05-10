effc664185f180cf71a2685722796862bc7e083e
project-maec <82032847+project-maec@users.noreply.github.com>
2024-05-10 10:55:21 +0000

    updated

--- libflow/parser.py
+++ libflow/parser.py
@@ -24,9 +24,12 @@ class Parser:
         # This is a simplistic parser assuming well-formed input and does not handle errors
         if '(' in expression:
             name_expr = expression[:expression.find('(')]
+            is_ts = False
             if name_expr in function_map.keys():
                 # it's a function
                 data = function_map[name_expr]
+                if name_expr.startswith('ts_'):
+                    is_ts =True
             elif name_expr.isnumeric():
                 raise TypeError('numerical value is not callable')
             else:
@@ -36,6 +39,7 @@ class Parser:
             args = []
             depth = 0
             last_split = 0
+            
             # split arguments considering nested functions
             for i, char in enumerate(inside_parenthesis):
                 if char == '(':
@@ -46,7 +50,7 @@ class Parser:
                     args.append(self.parse(inside_parenthesis[last_split:i].strip()))
                     last_split = i + 1
             args.append(self.parse(inside_parenthesis[last_split:].strip()))
-            return Node(data, args)
+            return Node(data, args, is_ts)
         else:
             if expression.isnumeric():
                 expression=float(expression)
