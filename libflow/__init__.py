402895543b70b4dc05c60cf8aae8e74b8b7b8b2d
project-maec <82032847+project-maec@users.noreply.github.com>
2024-05-10 08:10:14 +0000

    updated

---
+++ libflow/__init__.py
@@ -0,0 +1,13 @@
+"""
+Expression based sim tool for equity statarb
+
+Author: projectmaec
+
+References:
+[1] https://gplearn.readthedocs.io/en/stable/
+[2] https://github.com/UePG-21/gpquant
+"""
+
+__version__ = "0.1.6"
+
+__all__ = ["function", "fitness", "parser", "syntax_tree"]
