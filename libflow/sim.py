3de14ff18582d70068945b92efa188c1dfcb6a7f
project-maec <82032847+project-maec@users.noreply.github.com>
2024-05-16 10:12:00 +0000

    updated

--- libflow/sim.py
+++ libflow/sim.py
@@ -9,6 +9,8 @@ import exchange_calendars as xcals
 import glob
 import datetime
 
+from multiprocessing import Pool
+from functools import partial
 
 from .function import Function
 from .function import function_map
@@ -44,7 +46,6 @@ class Simflow:
         self.l_train_data = None
         self.l_signal_data = None
 
-
     def set_target(self, Y):
         """
         @param Y: pd series for the required dates
@@ -54,7 +55,7 @@ class Simflow:
 
     def add_alpha(self, alpha: str, alpha_name: str):
         alpha_expr = self.expr_parser.parse(alpha)
-        self.signal_dict[alpha_name]= alpha_expr
+        self.signal_dict[alpha_name] = alpha_expr
         return
 
     def get_required_fields(self):
@@ -62,40 +63,39 @@ class Simflow:
         for a in self.signal_dict.values():
             l_alpha_nodes = preorder_traversal(a)
             for e in l_alpha_nodes:
-                if isinstance(e,str):
+                if isinstance(e, str):
                     list_fields.append(e)
         return np.unique(list_fields).tolist()
-    
 
     def get_in_sample_dates(self, dates):
         df_dates = pd.Series(dates)
         if self.is_period == 'alt_m':
-            return df_dates[(df_dates.dt.quarter + df_dates.dt.year)%2 != 0].tolist()
+            return df_dates[(df_dates.dt.quarter + df_dates.dt.year) % 2 != 0].tolist()
         elif self.is_period == 'alt_q':
-            return df_dates[(df_dates.dt.month + df_dates.dt.year)%2 != 0].tolist()
+            return df_dates[(df_dates.dt.month + df_dates.dt.year) % 2 != 0].tolist()
         elif '-' in self.is_period:
             return generate_date_list(self.is_period, df_dates)
         else:
             return dates
-        
+
     def load_train_data(self, X_train):
         required_flds = self.get_required_fields()
-        if len(X_train.index.names)>1:
+        if len(X_train.index.names) > 1:
             X = X_train.reset_index()
         l_train_data = {}
         for x in required_flds:
-            l_train_data[x] = X.pivot(index='date',columns='ticker',values=x)
+            l_train_data[x] = X.pivot(index='date', columns='ticker', values=x)
         self.l_train_data = l_train_data
 
-    def compute_all(self,X):
+    def compute_all(self, X):
         l_signal_data = {}
         if self.l_signal_data is None:
             self.load_train_data(X)
 
         for sname in self.signal_dict.keys():
-            if self.verbose>0:
+            if self.verbose > 0:
                 print(f'computing {sname}')
-            l_signal_data[sname] =  self.signal_dict[sname](self.l_train_data)
+            l_signal_data[sname] = self.signal_dict[sname](self.l_train_data)
         self.l_signal_data = l_signal_data
         return
 
@@ -105,14 +105,35 @@ class Simflow:
             raise ValueError(f'{sname}.index is different from Y.index')
         if not signal_data.columns.equals(Y.columns):
             raise ValueError(f'{sname}.columns is different from Y.columns')
-        
-        return
 
-map_cal2mic={
-    "US":["XNAS"],
-    "JP":["XTKS"],
-    "CN":["XSHG"]
+        is_dates = self.get_in_sample_dates(signal_data.index)
+        ic_series = row_wise_corr(signal_data.values,Y.values)
+        ic_series.index = signal_data.index
+        ic_series = ic_series[ic_series.index.isin(is_dates)]
+
+        return ic_series
+    
+    def score_os(self, sname, Y):
+        signal_data = self.l_signal_data[sname]
+        if not signal_data.index.equals(Y.index):
+            raise ValueError(f'{sname}.index is different from Y.index')
+        if not signal_data.columns.equals(Y.columns):
+            raise ValueError(f'{sname}.columns is different from Y.columns')
+
+        # is_dates = self.get_in_sample_dates(signal_data.index)
+        ic_series = row_wise_corr(signal_data.values,Y.values)
+        ic_series.index = signal_data.index
+        # ic_series = ic_series[ic_series.index.isin(is_dates)]
+
+        return ic_series
+
+
+map_cal2mic = {
+    "US": ["XNAS"],
+    "JP": ["XTKS"],
+    "CN": ["XSHG"]
 }
+
 
 def get_holidays_by_calendar(calendar='US'):
     exch_list = map_cal2mic[calendar]
@@ -124,10 +145,11 @@ def get_holidays_by_calendar(calendar='US'):
 def get_holidays_by_exch(exch_mic):
     file_patts = f'/home/ubuntu/data/calendar/holidays/{exch_mic}_latest.csv'
     files = glob.glob(file_patts)
-    if len(files)<=0:
-        return pd.DataFrame([], columns=['date','exchange'])
+    if len(files) <= 0:
+        return pd.DataFrame([], columns=['date', 'exchange'])
     else:
         return pd.read_csv(files[-1])
+
 
 def preorder_traversal(node):
     list_elements = []
@@ -141,26 +163,41 @@ def preorder_traversal(node):
 def split_time_periods(periods_str):
     # Split the input string by semicolon to get individual periods
     periods = periods_str.split(';')
-    
+
     # Initialize a list to hold the tuples of (start, end) dates
     time_periods = []
-    
+
     # Iterate over each period and split by hyphen to get start and end dates
     for period in periods:
         start_date, end_date = period.split('-')
         time_periods.append((start_date, end_date))
-    
+
     return time_periods
 
 
-def generate_date_list(periods_str, dates:pd.Series):
+def generate_date_list(periods_str, dates: pd.Series):
     time_periods = split_time_periods(periods_str)
     all_dates = []
-    
+
     for start_str, end_str in time_periods:
         # Convert string dates to datetime objects
         start_date = datetime.strptime(start_str, '%Y%m%d')
         end_date = datetime.strptime(end_str, '%Y%m%d')
-        all_dates.append(dates[dates.between(start_date,end_date)].tolist())
+        all_dates.append(dates[dates.between(start_date, end_date)].tolist())
 
     return all_dates
+
+
+def compute_spearman_corr(row_index, arr1, arr2):
+    return [row_index, stats.spearmanr(arr1[row_index], arr2[row_index], nan_policy='omit')[0]]
+
+
+def row_wise_corr(arr1, arr2, num_workers=4):
+    # row_correlations = np.zeros(arr1.shape[0])
+    with Pool(num_workers) as pool:
+        res = pool.map(partial(compute_spearman_corr, arr1=arr1,
+                       arr2=arr2), range(arr1.shape[0]))
+        pool.close()
+    df_res = pd.DataFrame(
+        res, columns=['idx', 'val']).sort_values('idx')['val']
+    return pd.Series(df_res.values,name='daily_ic')
